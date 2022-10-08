5e761e05c680a86f0754dc8c6528ae6fa3688b7f
Dicky Darmawan <dicky93darmawan@gmail.com>
2022-10-08 15:57:04 +0800

    p

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 require("./app/routes/routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
